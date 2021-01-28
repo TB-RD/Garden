304b3e7b3d2ee3de2d514cde02b4e3bbfcc5d388
TB-RD <jaguar1010jg@gmail.com>
2021-01-28 22:45:56 +0300

    up time

--- bot.js
+++ bot.js
@@ -38,3 +38,13 @@ async function giveGif(message) {
                
     }
 }
+
+
+let count = 0;
+setInterval(
+  () =>
+    require("node-fetch")(process.env.URL).then(() =>
+      console.log(`[${++count}] here i pinged ${process.env.URL}`)
+    ),
+  300000
+);
